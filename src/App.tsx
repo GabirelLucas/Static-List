71f49610b520f80c4736aca2d15b3675532228ff
Gabriel Nunes <gabriel_leirbag_@outlook.com>
2025-04-11 12:02:51 -0300

    removendo cores personalizadas

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import TaskList from "./components/taskList";
 
 const App: React.FC = () => {
   return (
-    <div className="max-w-xl my-16 mx-auto p-8 bg-gray500 rounded-lg">
-      <h1 className="text-blue text-4xl font-bold text-center mb-8 ">
+    <div className="max-w-xl my-16 mx-auto p-8 bg-stone-800 rounded-lg">
+      <h1 className="text-blue-400 text-4xl font-bold text-center mb-8 ">
         Lista de Tarefas
       </h1>
       <TaskList />
--- src/components/taskList/index.tsx
+++ src/components/taskList/index.tsx
@@ -19,26 +19,26 @@ const TaskList: React.FC = () => {
       {tasks.map((task) => (
         <div
           key={task.id}
-          className={`flex items-center bg-gray700 p-4 rounded-2 gap-4`}
+          className={`flex items-center bg-zinc-950 p-4 rounded-2 gap-4`}
         >
           <button
             onClick={() => toggleTask(task.id)}
-            className={`bg-transparent border-none ${task.completed ? "text-purpleDark" : "text-purple"} cursor-pointer`}
+            className={`bg-transparent border-none ${task.completed ? "text-violet-500" : "text-purple-400"} cursor-pointer`}
           >
             {task.completed ? (
               <CheckCircle
                 size={24}
                 weight="fill"
-                className="text-purpleDark"
+                className="text-violet-500"
               />
             ) : (
-              <Circle size={24} className="text-purpleDark" />
+              <Circle size={24} className="text-violet-500" />
             )}
           </button>
 
           <div className="flex flex-col">
-            <strong className="text-gray100 font-bold">{task.title}</strong>
-            <span className="text-gray300">{task.description}</span>
+            <strong className="text-slate-100 font-bold">{task.title}</strong>
+            <span className="text-neutral-500">{task.description}</span>
           </div>
         </div>
       ))}
