6d09567e119411ab6fdcd5a489c234f16931e1cb
Gabriel Nunes <gabriel_leirbag_@outlook.com>
2025-04-12 15:05:01 -0300

    corrigindo bug no botão de concluído

--- src/components/sortableItem/index.tsx
+++ src/components/sortableItem/index.tsx
@@ -22,22 +22,21 @@ const SortableItem: React.FC<SortableItemProps> = ({
       style={style}
       className="flex items-center bg-zinc-950 p-4 rounded-2 gap-4 select-none border border-transparent hover:border-zinc-400 hover:opacity-80 cursor-grab active:cursor-grabbing "
     >
+      <button
+        onClick={() => toggleTask(task.id)}
+        className={`bg-transparent border-none ${task.completed ? "text-violet-500" : "text-purple-400"} cursor-pointer`}
+      >
+        {task.completed ? (
+          <CheckCircle size={24} weight="fill" className="text-violet-500" />
+        ) : (
+          <Circle size={24} className="text-violet-500" />
+        )}
+      </button>
       <div
         {...attributes}
         {...listeners}
         className="flex items-center gap-4 grow cursor-grab active:cursor-grabbing"
       >
-        <button
-          onClick={() => toggleTask(task.id)}
-          className={`bg-transparent border-none ${task.completed ? "text-violet-500" : "text-purple-400"} cursor-pointer`}
-        >
-          {task.completed ? (
-            <CheckCircle size={24} weight="fill" className="text-violet-500" />
-          ) : (
-            <Circle size={24} className="text-violet-500" />
-          )}
-        </button>
-
         <div className="flex flex-col">
           <strong className="text-slate-100 font-bold">{task.title}</strong>
           <span className="text-neutral-500">{task.description}</span>
