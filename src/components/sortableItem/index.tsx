8231d42261d59117a1e47c20d2bad67e38e0ce7f
Gabriel Nunes <gabriel_leirbag_@outlook.com>
2025-04-12 15:02:01 -0300

    card totalmente arrastável

--- src/components/sortableItem/index.tsx
+++ src/components/sortableItem/index.tsx
@@ -20,34 +20,34 @@ const SortableItem: React.FC<SortableItemProps> = ({
     <div
       ref={setNodeRef}
       style={style}
-      className="flex items-center bg-zinc-950 p-4 rounded-2 gap-4 select-none relative group border border-transparent hover:border-zinc-400 hover:opacity-80 cursor-grab focus-within:border-dashed"
+      className="flex items-center bg-zinc-950 p-4 rounded-2 gap-4 select-none border border-transparent hover:border-zinc-400 hover:opacity-80 cursor-grab active:cursor-grabbing "
     >
-      {/* Invisible draggable layer */}
       <div
         {...attributes}
         {...listeners}
-        className="absolute inset-0 z-0 cursor-grab active:cursor-grabbing"
-      />
-
-      <button
-        onClick={() => toggleTask(task.id)}
-        className={`bg-transparent border-none ${task.completed ? "text-violet-500" : "text-purple-400"} cursor-pointer z-10`}
+        className="flex items-center gap-4 grow cursor-grab active:cursor-grabbing"
       >
-        {task.completed ? (
-          <CheckCircle size={24} weight="fill" className="text-violet-500" />
-        ) : (
-          <Circle size={24} className="text-violet-500" />
-        )}
-      </button>
+        <button
+          onClick={() => toggleTask(task.id)}
+          className={`bg-transparent border-none ${task.completed ? "text-violet-500" : "text-purple-400"} cursor-pointer`}
+        >
+          {task.completed ? (
+            <CheckCircle size={24} weight="fill" className="text-violet-500" />
+          ) : (
+            <Circle size={24} className="text-violet-500" />
+          )}
+        </button>
 
-      <div className="flex flex-col z-10">
-        <strong className="text-slate-100 font-bold">{task.title}</strong>
-        <span className="text-neutral-500">{task.description}</span>
+        <div className="flex flex-col">
+          <strong className="text-slate-100 font-bold">{task.title}</strong>
+          <span className="text-neutral-500">{task.description}</span>
+        </div>
       </div>
 
+      {/* Botão fora da área de arrasto */}
       <button
         onClick={() => deleteTask(task.id)}
-        className="ml-auto rounded-2xl p-1 text-red-500 hover:text-red-400 hover:bg-neutral-700 hover:rounded-2xl hover:p-1 active:scale-95 cursor-pointer select-none transition-transform z-10"
+        className="ml-auto rounded-2xl p-1 text-red-500 hover:text-red-400 hover:bg-neutral-700 active:scale-95 cursor-pointer transition-transform"
         title="Excluir tarefa"
       >
         <Trash size={20} />
